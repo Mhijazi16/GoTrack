36620afc82eead72b69112a0346ed3117ce044c2
mhijazi16 <gultimate0@gmail.com>
2025-05-24 13:54:22 +0300

    fixes

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type Task struct {
-	Name        string
-	Description string
-	Status      string
+	ID          uuid.UUID `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
 func main() {
+	task := Task{Name: "say hello2", Description: "some description", Status: "Done"}
+	runOperation(task.addTasks)
 	runOperation(displayTasks)
 }
--- operation.go
+++ operation.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 const filename string = "tasks.json"
@@ -29,5 +32,28 @@ func displayTasks(file *os.File) {
 
 	for _, task := range tasks {
 		fmt.Println(task)
+	}
+}
+
+func (newTask *Task) addTasks(file *os.File) {
+
+	var tasks []Task
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&tasks); err != nil {
+		log.Fatal("failed to parse json file")
+	}
+
+	newTask.ID = uuid.New()
+	newTask.CreatedAt = time.Now()
+	newTask.UpdatedAt = time.Now()
+	tasks = append(tasks, *newTask)
+
+	data, err := json.MarshalIndent(tasks, "", "  ")
+	if err != nil {
+		log.Fatal("failed to update tasks")
+	}
+
+	if err := os.WriteFile(filename, data, 0644); err != nil {
+		log.Fatal("failed to update tasks")
 	}
 }
